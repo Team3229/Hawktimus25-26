00657c702e3acf4537911d4f2563292fca8d680b
CptPhoenix <reillyhaggard@gmail.com>
2026-01-20 16:55:23 -0500

    Made commands for the intake subsytem
    Co-authored-by: HaloOwen <haloowen@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -44,7 +44,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.hawklibraries.utilities.Alliance;
-import frc.hawklibraries.utilities.Alliance.AllianceColor;
+import frc.hawklibraries.utilities.Alliance.AllianceColor;	
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.utilities.LimelightHelpers.PoseEstimate;
 
--- src/main/java/frc/robot/subsystems/manipSubsystems/intakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipSubsystems/intakeSubsystem.java
@@ -5,10 +5,11 @@ import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
@@ -20,79 +21,81 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.manipSubsystems.IntakeSubsystem;
 
-public class intakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
 
     private static SparkMax armMotor;
+    private static SparkMax rodMotor;
     private static SparkMaxConfig armMotorConfig;
+    private static SparkMaxConfig rodMotorConfig;
     private static ArmFeedforward feedForward;
     private static ProfiledPIDController pidController;
 
-    private static final int CAN_ID = 9;
+    private static final int ARM_CAN_ID = 9; // TODO: change this
+    private static final int ROD_CAN_ID = 8; // TODO: change this
     private static final double POSITION_CONVERSION_FACTOR = 2 * Math.PI;
 
-    public static final Angle HOME_ANGLE = Degrees.of(345);
-    public static final Angle COLLECTION_POINT = Degrees.of(270);
+    public static final Angle HOME_ANGLE = Degrees.of(345); // TODO: change this
+    public static final Angle COLLECTION_POINT = Degrees.of(270); // TODO: change this
 
-    private intakeSubsystem arm;
-    private intakeSubsystem intakeRod1;
-    private intakeSubsystem intakeRod2;
+    private static final double kP = 0; // TODO: change this
+    private static final double kI = 0; // TODO: change this
+    private static final double kD = 0; // TODO: change this
 
-    private static final double kP = 0;
-    private static final double kI = 0;
-    private static final double kD = 0;
+    private static final double CW_SPEED = 0; // TODO: change this
+    private static final double CCW_SPEED = 0; // TODO: change this
 
-    private static final AngularVelocity MAX_VELOCITY = RadiansPerSecond.of(0);
-    private static final AngularAcceleration MAX_ACCELERATION = RadiansPerSecondPerSecond.of(0);
+    private static final AngularVelocity MAX_VELOCITY = RadiansPerSecond.of(0); // TODO: change this
+    private static final AngularAcceleration MAX_ACCELERATION = RadiansPerSecondPerSecond.of(0); // TODO: change this
 
-    private static final Angle POSITION_TOLERANCE = Degrees.of(0);
-    private static final AngularVelocity VELOCITY_TOLERANCE = RadiansPerSecond.of(0);
+    private static final Angle POSITION_TOLERANCE = Degrees.of(0); // TODO: change this
+    private static final AngularVelocity VELOCITY_TOLERANCE = RadiansPerSecond.of(0); // TODO: change this
 
+    public static void IntakeSubsytem() {
 
-    public static void intakeSubsytem() {   
+        armMotor = new SparkMax(ARM_CAN_ID, MotorType.kBrushless);
 
-        armMotor = new SparkMax(CAN_ID, MotorType.kBrushless);
+        rodMotor = new SparkMax(ROD_CAN_ID, MotorType.kBrushless);
 
         armMotorConfig = new SparkMaxConfig();
 
+        rodMotorConfig = new SparkMaxConfig();
+
         feedForward = new ArmFeedforward(
-            0,
-            0,
-            0,
-            0
+                0, // TODO: change this
+                0, // TODO: change this
+                0, // TODO: change this
+                0 // TODO: change this
         );
 
         pidController = new ProfiledPIDController(
-            kP,
-            kI,
-            kD,
-            new Constraints(
-                MAX_VELOCITY.in(RadiansPerSecond),
-                MAX_ACCELERATION.in(RadiansPerSecondPerSecond)
-            )
-        );
+                kP,
+                kI,
+                kD,
+                new Constraints(
+                        MAX_VELOCITY.in(RadiansPerSecond),
+                        MAX_ACCELERATION.in(RadiansPerSecondPerSecond)));
 
         pidController.setTolerance(
-            POSITION_TOLERANCE.in(Radians),
-            VELOCITY_TOLERANCE.in(RadiansPerSecond)
-        );
+                POSITION_TOLERANCE.in(Radians),
+                VELOCITY_TOLERANCE.in(RadiansPerSecond));
 
         setSetpoint(HOME_ANGLE);
         pidController.reset(getPosition().in(Radians));
 
         armMotorConfig.absoluteEncoder
-            .positionConversionFactor(POSITION_CONVERSION_FACTOR)
-            .velocityConversionFactor(POSITION_CONVERSION_FACTOR)
-            .zeroCentered(true);
+                .positionConversionFactor(POSITION_CONVERSION_FACTOR)
+                .velocityConversionFactor(POSITION_CONVERSION_FACTOR)
+                .zeroCentered(true);
 
         armMotor.configure(
-            armMotorConfig,
-            ResetMode.kNoResetSafeParameters,
-            PersistMode.kNoPersistParameters
-        );
+                armMotorConfig,
+                ResetMode.kNoResetSafeParameters,
+                PersistMode.kNoPersistParameters);
     }
 
-     public static Angle getPosition() {
+    public static Angle getPosition() {
         return Radians.of(armMotor.getAbsoluteEncoder().getPosition());
     }
 
@@ -102,31 +105,53 @@ public class intakeSubsystem extends SubsystemBase {
 
     public Command rotateTo(Angle setpoint) {
         return runOnce(
-            () -> setSetpoint(setpoint)
-        ).until(
-            () -> atGoal()
-        );
+                () -> setSetpoint(setpoint)).until(
+                        () -> atGoal());
     }
 
-    public Command spin(boolean clockwise) {
+    /**
+     * creates a command to collect the fuel by spinning the rod
+     * 
+     * @return Command to spin rod
+     */
+    public Command intake() {
         return Commands.runOnce(
-            () -> setRodDirection(clockwise)
+                () -> rodMotor.set(CW_SPEED) // TODO: Check direction
         );
     }
 
-    private void setWheelDirection(boolean clockwise) {
-
-        if (clockwise) {
-            wheelMotor.set(CW_SPEED);
-        } else {
-            wheelMotor.set(CCW_SPEED);
-        }
-
+    /**
+     * creates a command to reverse the intake to put the fuel into the human player station 
+     * @return Command to spin the rod in reverse
+     */
+    public Command extake() {
+        return Commands.runOnce(
+                () -> rodMotor.set(CCW_SPEED) // TODO: Check direction
+        );
     }
+
+    /**
+     * creates a command that pushes the intake arm down to the collection point
+     * and pushes the storage area out.
+     * 
+     * @return Command to rotate the arm
+     */
+    public Command extendIntake() {
+        return rotateTo(COLLECTION_POINT);
+    }
+    /**
+     * creates a command that pulls the intake arm back to the 
+     * home point in order to move the fuel in storage.
+     * @return Command to pull arm back
+     */
+    public Command agitateFuel() {
+        return rotateTo(HOME_ANGLE);
+    }
+    
     public double getDraw() {
-        return wheelMotor.getOutputCurrent();
+        return rodMotor.getOutputCurrent();
     }
-
+    //
     private static void setSetpoint(Angle setpoint) {
         pidController.setGoal(setpoint.in(Radians));
     }
@@ -138,34 +163,10 @@ public class intakeSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         armMotor.setVoltage(
-            pidController.calculate(getPosition().in(Radians)) +
-            feedForward.calculate(
-                pidController.getSetpoint().position,
-                pidController.getSetpoint().velocity
-            )
-        );
+                pidController.calculate(getPosition().in(Radians)) +
+                        feedForward.calculate(
+                                pidController.getSetpoint().position,
+                                pidController.getSetpoint().velocity));
     }
 
-        //intake/storage push out function
-        
-        public Command extendIntake() {
-            return arm.rotateTo(intakeSubsystem.COLLECTION_POINT);
-        }
-
-        //turn intake on function
-
-        public Command intakeCommand() {
-            return intakeRod1.spin();
-            .alongWith(intakeRod2.spin());
-        }
-
-
-        //roller indexer function (runs while intaking or toggle on/off)
-
-        //extake (run intake and indexer in reverse) for human player station and in panic moments
-        
-        //intake push function
-
-    
 }
-
