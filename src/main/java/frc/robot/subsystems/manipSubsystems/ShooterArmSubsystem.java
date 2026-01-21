337430c10da33d5d9a1ddce37ba30aa4dd3fafae
Jimmy737309 <j5cc0109@gmail.com>
2026-01-21 16:54:24 -0500

    hopefully fixing the file names

--- src/main/java/frc/robot/subsystems/manipSubsystems/ShooterArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipSubsystems/ShooterArmSubsystem.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterArmSubsystem extends SubsystemBase {
-
+// this is a comment!
     private SparkMax armMotor;
     private SparkMaxConfig armMotorConfig;
     private ArmFeedforward feedForward;
