135933411d2f6a768ebfc4ef58f86a7434e50c6f
Jimmy737309 <j5cc0109@gmail.com>
2026-01-23 19:56:44 -0500

    fixing intake

--- src/main/java/frc/robot/subsystems/manipSubsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipSubsystems/IntakeSubsystem.java
@@ -1,16 +1,12 @@
 package frc.robot.subsystems.manipSubsystems;
 
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Radians;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
-
-import com.revrobotics.PersistMode;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -18,19 +14,29 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.manipSubsystems.IntakeSubsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
 public class IntakeSubsystem extends SubsystemBase {
 
-    private static SparkMax armMotor;
-    private static SparkMax rodMotor;
-    private static SparkMaxConfig armMotorConfig;
-    private static SparkMaxConfig rodMotorConfig;
+    private static TalonFX armMotor;
+    private static TalonFX rodMotor;
+    private static CANcoder armCanMotor;
+    private static TalonFXConfiguration armMotorConfig;
+    private static TalonFXConfiguration rodMotorConfig;
+    private static CANcoderConfiguration armCanCoderConfig;
+
+
     private static ArmFeedforward feedForward;
-    private static ProfiledPIDController pidController;
+    private static ProfiledPIDController armPIDController;
 
     private static final int ARM_CAN_ID = 9; // TODO: change this
     private static final int ROD_CAN_ID = 8; // TODO: change this
@@ -39,79 +45,92 @@ public class IntakeSubsystem extends SubsystemBase {
     public static final Angle HOME_ANGLE = Degrees.of(345); // TODO: change this
     public static final Angle COLLECTION_POINT = Degrees.of(270); // TODO: change this
 
-    private static final double kP = 0; // TODO: change this
-    private static final double kI = 0; // TODO: change this
-    private static final double kD = 0; // TODO: change this
+    private static final double aP = 0; // TODO: change this
+    private static final double aI = 0; // TODO: change this
+    private static final double aD = 0; // TODO: change this
+
+    private static final double rP = 0; // TODO: change this
+    private static final double rI = 0; // TODO: change this
+    private static final double rD = 0; // TODO: change this
 
     private static final double CW_SPEED = 0; // TODO: change this
     private static final double CCW_SPEED = 0; // TODO: change this
 
-    private static final AngularVelocity MAX_VELOCITY = RadiansPerSecond.of(0); // TODO: change this
-    private static final AngularAcceleration MAX_ACCELERATION = RadiansPerSecondPerSecond.of(0); // TODO: change this
+    private static final AngularVelocity MAX_VELOCITY = DegreesPerSecond.of(0); // TODO: change this
+    private static final AngularAcceleration MAX_ACCELERATION = DegreesPerSecondPerSecond.of(0); // TODO: change this
 
     private static final Angle POSITION_TOLERANCE = Degrees.of(0); // TODO: change this
-    private static final AngularVelocity VELOCITY_TOLERANCE = RadiansPerSecond.of(0); // TODO: change this
-   
-   //variable for elastic
-    public static boolean intaking = false;
+    private static final AngularVelocity VELOCITY_TOLERANCE = DegreesPerSecond.of(0); // TODO: change this
 
     public IntakeSubsystem() {
 
-        armMotor = new SparkMax(ARM_CAN_ID, MotorType.kBrushless);
+        CANcoderConfiguration armCanCoderConfig = new CANcoderConfiguration();
+        armCanCoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0; //TODO: Change
+        armCanCoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+        armCanCoderConfig.MagnetSensor.MagnetOffset = 0.0;
+        armCanMotor.getConfigurator().apply(armCanCoderConfig);
 
-        rodMotor = new SparkMax(ROD_CAN_ID, MotorType.kBrushless);
+        TalonFXConfiguration armMotorConfig = new TalonFXConfiguration();
+        armMotorConfig.Feedback.FeedbackRemoteSensorID = armCanMotor.getDeviceID();
+        armMotorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.SyncCANcoder;
+        armMotorConfig.Feedback.SensorToMechanismRatio = 0.0; //TODO: Change
+        armMotorConfig.Feedback.RotorToSensorRatio = 0.0; //TODO: Change
 
-        armMotorConfig = new SparkMaxConfig();
+        armMotor.getConfigurator().apply(armMotorConfig);
 
-        rodMotorConfig = new SparkMaxConfig();
+        armCanMotor= new CANcoder(ARM_CAN_ID);
+
+        armMotor = new TalonFX(ARM_CAN_ID, "Placeholder");
+
+        rodMotor = new TalonFX(ROD_CAN_ID, "Placeholder");
+
+        armMotorConfig = new TalonFXConfiguration();
+
+        rodMotorConfig = new TalonFXConfiguration();
 
         feedForward = new ArmFeedforward(
-                0, // TODO: change this
-                0, // TODO: change this
-                0, // TODO: change this
-                0 // TODO: change this
+            0, // TODO: change this
+            0, // TODO: change this
+            0, // TODO: change this
+            0 // TODO: change this
         );
 
-        pidController = new ProfiledPIDController(
-                kP,
-                kI,
-                kD,
-                new Constraints(
-                        MAX_VELOCITY.in(RadiansPerSecond),
-                        MAX_ACCELERATION.in(RadiansPerSecondPerSecond)));
+        rodMotorConfig.Slot0.kP = (rP);
+        rodMotorConfig.Slot0.kI = (rI);
+        rodMotorConfig.Slot0.kD = (rD);
+        
+        armPIDController = new ProfiledPIDController(
+            aP,
+            aI,
+            aD,
+            new Constraints(
+                MAX_VELOCITY.in(DegreesPerSecond),
+                MAX_ACCELERATION.in(DegreesPerSecondPerSecond)
+            )
+        );
 
-        pidController.setTolerance(
-                POSITION_TOLERANCE.in(Radians),
-                VELOCITY_TOLERANCE.in(RadiansPerSecond));
+        armPIDController.setTolerance(
+            POSITION_TOLERANCE.in(Degrees),
+            VELOCITY_TOLERANCE.in(DegreesPerSecond)
+        );
 
         setSetpoint(HOME_ANGLE);
-        pidController.reset(getPosition().in(Radians));
-
-        armMotorConfig.absoluteEncoder
-                .positionConversionFactor(POSITION_CONVERSION_FACTOR)
-                .velocityConversionFactor(POSITION_CONVERSION_FACTOR)
-                .zeroCentered(true);
-
-        armMotor.configure(
-                armMotorConfig,
-                ResetMode.kNoResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-
-        SmartDashboard.putBoolean("Intaking", intaking);
+            armPIDController.reset(getPosition().in(Degrees)
+        );
     }
 
     public static Angle getPosition() {
-        return Radians.of(armMotor.getAbsoluteEncoder().getPosition());
+        return Degrees.of(armCanMotor.getAbsolutePosition().getValueAsDouble());
     }
 
     public static AngularVelocity getVelocity() {
-        return RadiansPerSecond.of(armMotor.getAbsoluteEncoder().getVelocity());
+        return DegreesPerSecond.of(armCanMotor.getAbsolutePosition().getValueAsDouble());
     }
 
     public Command rotateTo(Angle setpoint) {
         return runOnce(
-                () -> setSetpoint(setpoint)).until(
-                        () -> atGoal());
+            () -> setSetpoint(setpoint)).until(
+                () -> atGoal());
     }
 
     /**
@@ -120,8 +139,9 @@ public class IntakeSubsystem extends SubsystemBase {
      * @return Command to spin rod
      */
     public Command intake() {
-        return Commands.runOnce(() -> rodMotor.set(CW_SPEED)) // TODO: Check direction()-> intaking  true;
-        .andThen(Commands.runOnce(() -> intaking = true));
+        return Commands.runOnce(
+            () -> rodMotor.set(CW_SPEED) // TODO: Check direction
+        );
     }
 
     /**
@@ -130,7 +150,7 @@ public class IntakeSubsystem extends SubsystemBase {
      */
     public Command extake() {
         return Commands.runOnce(
-                () -> rodMotor.set(CCW_SPEED) // TODO: Check direction
+            () -> rodMotor.set(CCW_SPEED) // TODO: Check direction
         );
     }
 
@@ -149,30 +169,32 @@ public class IntakeSubsystem extends SubsystemBase {
      * @return Command to pull arm back
      */
     public Command agitateFuel() {
-        return rotateTo(HOME_ANGLE);
+        return rotateTo(HOME_ANGLE)
+        .andThen(new WaitCommand(0.5))
+        .andThen(rotateTo(COLLECTION_POINT)); //TODO: Should work but will need a controlled test
     }
     
-    public double getDraw() {
-        return rodMotor.getOutputCurrent();
+    public StatusSignal<Current> getDraw() {
+        return rodMotor.getMotorStallCurrent();
     }
-    //
+    
     private static void setSetpoint(Angle setpoint) {
-        pidController.setGoal(setpoint.in(Radians));
+        armPIDController.setGoal(setpoint.in(Degrees));
     }
 
     private boolean atGoal() {
-        return pidController.atGoal();
+        return armPIDController.atGoal();
     }
 
     @Override
     public void periodic() {
         armMotor.setVoltage(
-                pidController.calculate(getPosition().in(Radians)) +
-                        feedForward.calculate(
-                                pidController.getSetpoint().position,
-                                pidController.getSetpoint().velocity));
+            armPIDController.calculate(getPosition().in(Degrees)) +
+                feedForward.calculate(
+                    armPIDController.getSetpoint().position,
+                    armPIDController.getSetpoint().velocity
+                )
+            );
     }
-    
-    SmartDashboard.putBoolean("Intaking", intaking);
 
 }
