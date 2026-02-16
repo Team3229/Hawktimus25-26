e96029dda5982ef3995e15810854d3ac78365c12
Jimmy737309 <j5cc0109@gmail.com>
2026-02-16 12:41:01 -0500

    fixing the package (i think zane still can't see errors)

--- src/main/java/frc/robot/subsystems/manipSubsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipSubsystems/LEDSubsystem.java
@@ -1,4 +1,4 @@
-package main.java.frc.robot.subsystems.manipSubsystems;
+package frc.robot.subsystems.manipSubsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
