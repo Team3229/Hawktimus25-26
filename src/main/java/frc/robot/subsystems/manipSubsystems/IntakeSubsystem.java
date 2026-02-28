9d3fd2b46e221f77cd473b6ad0de1623dbd2eb34
Jimmy737309 <j5cc0109@gmail.com>
2026-02-28 11:52:35 -0500

    wait i meant setting a default command (this was removing warnings)

--- src/main/java/frc/robot/subsystems/manipSubsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipSubsystems/IntakeSubsystem.java
@@ -29,23 +29,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-import static edu.wpi.first.units.Units.Centimeters;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.Percent;
-import static edu.wpi.first.units.Units.Second;
-import static edu.wpi.first.units.Units.Seconds;
-
-import java.util.Map;
-
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class IntakeSubsystem extends SubsystemBase {
 
     private static TalonFX armMotorRight;
