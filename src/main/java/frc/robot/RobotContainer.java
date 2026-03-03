b854ca8aa8f96c42c73af4c4feaffaf390dcf82a
3229 Software <reillyhaggard@gmail.com>
2026-03-03 17:50:53 -0500

    approved :)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -188,6 +188,10 @@ public class RobotContainer {
 			spitterSubsystem.downFRPSCommand()
 		);
 
+		manipController.b_6().onTrue(
+			spitterSubsystem.setSpitterSpeed(hubAlign.distanceFromHub())
+		);
+
 	}
 
 	public void initTelemetery() {
