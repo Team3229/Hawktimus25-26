3450338b54847a1848e14cd36afbada90d606319
Kara Campbell <j5cc0109@gmail.com>
2026-03-12 19:39:13 -0400

    turns out the thing they reccommend to be on should be on!!! (alianceRelativeControl)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -98,7 +98,7 @@ public class RobotContainer {
 			.cubeTranslationControllerAxis(true)
 			.scaleTranslation(0.8)
 			.scaleRotation(0.9)
-			.allianceRelativeControl(false); // seems like it runs the same or better with this off
+			.allianceRelativeControl(true); // seems like it runs the same or better with this off
 			
 		driveSubsystem.setDefaultCommand(
 			driveSubsystem.driveFieldOriented(
