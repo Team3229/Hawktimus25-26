c77a1e6d686f3630547a9b761977a677adc29761
CptPhoenix <reillyhaggard@gmail.com>
2026-03-14 09:26:03 -0400

    Auto Stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -93,7 +93,7 @@ public class RobotContainer {
 		NamedCommands.registerCommand("ArmOut", manipSubsystem.intakeArmOut());
 		NamedCommands.registerCommand("WheelSpinUp", pathPlannerCommands.pathSpinUp());
 		NamedCommands.registerCommand("Shoot", pathPlannerCommands.pathShoot());
-		NamedCommands.registerCommand("Stow", manipSubsystem.stow());
+		NamedCommands.registerCommand("Stow", pathPlannerCommands.pathStow());
 
 		SwerveInputStream driveAngularVelocity = driveSubsystem.getInputStream(
 			() -> -driverController.a_Y(),
