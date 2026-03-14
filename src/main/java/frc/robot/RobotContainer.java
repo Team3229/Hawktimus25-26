789d3c51f503c29ecc5daaa66abb56fc44277d38
Kara Campbell <j5cc0109@gmail.com>
2026-03-13 22:52:16 -0400

    editing the cubic funcion of the controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,6 +64,12 @@ public class RobotContainer {
 
 	private void configureBindings() {
 
+		NamedCommands.registerCommand("Intake", pathPlannerCommands.pathIntake());
+		NamedCommands.registerCommand("ArmOut", manipSubsystem.intakeArmOut());
+		NamedCommands.registerCommand("WheelSpinUp", pathPlannerCommands.pathSpinUp());
+		NamedCommands.registerCommand("Shoot", pathPlannerCommands.pathShoot());
+		NamedCommands.registerCommand("Stow", manipSubsystem.stow());
+
 		DriverStation.silenceJoystickConnectionWarning(true);
 
 		configDriveControls();
@@ -83,12 +89,6 @@ public class RobotContainer {
 
 	private void configDriveControls() {
 
-		NamedCommands.registerCommand("Intake", pathPlannerCommands.pathIntake());
-		NamedCommands.registerCommand("ArmOut", manipSubsystem.intakeArmOut());
-		NamedCommands.registerCommand("WheelSpinUp", pathPlannerCommands.pathSpinUp());
-		NamedCommands.registerCommand("Shoot", pathPlannerCommands.pathShoot());
-		NamedCommands.registerCommand("Stow", manipSubsystem.stow());
-
 		SwerveInputStream driveAngularVelocity = driveSubsystem.getInputStream(
 			() -> -driverController.a_Y(),
 			() -> -driverController.a_X(),
@@ -97,7 +97,7 @@ public class RobotContainer {
 			.deadband(0.1)
 			.cubeRotationControllerAxis(true)
 			.cubeTranslationControllerAxis(true)
-			.scaleTranslation(0.8)
+			.scaleTranslation(0.6)
 			.scaleRotation(0.9)
 			.allianceRelativeControl(true);
 			
