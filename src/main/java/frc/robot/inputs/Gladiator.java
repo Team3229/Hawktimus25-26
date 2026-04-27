d8a9ca2f590311eeea9bdbeb21b451f80e7edeff
3229 Software <reillyhaggard@gmail.com>
2026-04-27 15:27:07 -0400

    added changes to pov and added 3 more axises yayyyy

--- src/main/java/frc/robot/inputs/Gladiator.java
+++ src/main/java/frc/robot/inputs/Gladiator.java
@@ -18,27 +18,29 @@ public class Gladiator {
     public double a_Y() {return applyDeadzone(controller.getRawAxis(1));}
     public double a_Z() {return applyDeadzone(controller.getRawAxis(2));}
     public double a_Throttle() {return controller.getRawAxis(3);}
+    public double a_Switch() {return controller.getRawAxis(4);}
+    public double a_Dial() {return controller.getRawAxis(5);}
 
     public Trigger b_Trigger() {return controller.button(1);}
     public Trigger b_FullTrigger() {return controller.button(2).and(controller.button(1));}
     public Trigger b_3() {return controller.button(3);}
     public Trigger b_4() {return controller.button(4);}
     public Trigger b_5() {return controller.button(5);}
-    public Trigger b_POV1Up() {return controller.button(6);}
-    public Trigger b_POV1Right() {return controller.button(7);}
-    public Trigger b_POV1Down() {return controller.button(8);}
-    public Trigger b_POV1Left() {return controller.button(9);}
-    public Trigger b_POV1Pushed() {return controller.button(10);}
-    public Trigger b_POV2Up() {return controller.button(11);}
-    public Trigger b_POV2Right() {return controller.button(12);}
-    public Trigger b_POV2Down() {return controller.button(13);}
-    public Trigger b_POV2Left() {return controller.button(14);}
-    public Trigger b_POV2Pushed() {return controller.button(15);}
-    public Trigger b_POV3Up() {return controller.button(16);}
-    public Trigger b_POV3Right() {return controller.button(17);}
-    public Trigger b_POV3Down() {return controller.button(18);}
-    public Trigger b_POV3Left() {return controller.button(19);}
-    public Trigger b_POV3Pushed() {return controller.button(20);}
+    public Trigger b_POV0Up() {return controller.button(6);}
+    public Trigger b_POV0Right() {return controller.button(7);}
+    public Trigger b_POV0Down() {return controller.button(8);}
+    public Trigger b_POV0Left() {return controller.button(9);}
+    public Trigger b_POV0Pushed() {return controller.button(10);}
+    public Trigger b_POV1Up() {return controller.button(11);}
+    public Trigger b_POV1Right() {return controller.button(12);}
+    public Trigger b_POV1Down() {return controller.button(13);}
+    public Trigger b_POV1Left() {return controller.button(14);}
+    public Trigger b_POV1Pushed() {return controller.button(15);}
+    public Trigger b_POV2Up() {return controller.button(16);}
+    public Trigger b_POV2Right() {return controller.button(17);}
+    public Trigger b_POV2Down() {return controller.button(18);}
+    public Trigger b_POV2Left() {return controller.button(19);}
+    public Trigger b_POV2Pushed() {return controller.button(20);}
     public Trigger b_TopTriggerDown() {return controller.button(22);}
     public Trigger b_TopTriggerUp() {return controller.button(21);}
 
