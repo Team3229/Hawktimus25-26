509cfdb00cd1dd6fb08c8892527d5cd8bad97273
Kara Campbell <j5cc0109@gmail.com>
2026-05-14 11:09:54 -0400

    prepping for the work for td!!

--- src/main/java/frc/robot/subsystems/MusicSubsystem.java
+++ src/main/java/frc/robot/subsystems/MusicSubsystem.java
@@ -1,25 +1,31 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 
 public class MusicSubsystem {
 
-    Orchestra m_orchestra = new Orchestra();
+    Orchestra m_orchestra;
+    StatusCode music;
     
     public MusicSubsystem(TalonFX talon) {
-        
+
+        m_orchestra = new Orchestra();
+
         m_orchestra.addInstrument(talon);
 
-        var music = m_orchestra.loadMusic("src/main/deploy/ImperialMarch.chrp");
+        music = m_orchestra.loadMusic("src/main/deploy/ImperialMarch.chrp");
 
-        if(!status.isOK()) {
-            System.out.println("Failed to load music: " + status.toString());
+        if(!music.isOK()) {
+            System.out.println("Failed to load music: " + music.toString());
         } else {
             m_orchestra.play();
         }
 
     }
+
+    // make this able to apply to all talons in a subsystem (not drive tho)
     
 }
