ba8cb342e5086a8b938e14843e5016a5e03cf197
3229 Software <owenzockoll@icloud.com>
2026-05-21 14:58:00 -0400

    added ctre to develop did merge conflicts everythings good yayyyy(hopefully)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.utilities.Elastic;
 
@@ -85,4 +86,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testExit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
